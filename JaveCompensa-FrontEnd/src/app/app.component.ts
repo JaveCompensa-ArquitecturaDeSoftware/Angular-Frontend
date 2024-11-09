a8f894fad58b6eeec8ede5003bc59b3006b9139d
Alberto Vigna <betoveluis@gmail.com>
2024-11-09 00:46:26 -0500

    Created and configured RouterLink.

--- JaveCompensa-FrontEnd/src/app/app.component.ts
+++ JaveCompensa-FrontEnd/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {NavBarComponent} from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
