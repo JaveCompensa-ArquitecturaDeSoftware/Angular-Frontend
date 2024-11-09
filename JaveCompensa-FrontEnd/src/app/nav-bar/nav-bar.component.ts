a080a749fddee39c2fcb66d2187e4ce766db406e
Alberto Vigna <betoveluis@gmail.com>
2024-11-09 00:41:12 -0500

    NavBar designed.

--- JaveCompensa-FrontEnd/src/app/nav-bar/nav-bar.component.ts
+++ JaveCompensa-FrontEnd/src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLinkActive
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
