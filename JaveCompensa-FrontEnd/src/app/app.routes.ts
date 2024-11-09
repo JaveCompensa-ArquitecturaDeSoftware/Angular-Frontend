c40505bf7c21d09893ab938dedcfedefa70ee6d8
Alberto Vigna <betoveluis@gmail.com>
2024-11-09 00:56:10 -0500

    Improved routing.

--- JaveCompensa-FrontEnd/src/app/app.routes.ts
+++ JaveCompensa-FrontEnd/src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
   },
+  {
+    path: 'loginRegister',
+    component: LoginRegisterComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'  // Redirección a 'home' por defecto
+  },
+  {
+    path: '**',
+    redirectTo: '/home'  // Ruta de fallback para rutas no encontradas
+  }
 ];
--- JaveCompensa-FrontEnd/src/app/nav-bar/nav-bar.component.ts
+++ JaveCompensa-FrontEnd/src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
-import {RouterLinkActive} from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    RouterLinkActive
+    RouterLinkActive,
+    RouterLink
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
