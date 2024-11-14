a27128770c142a1551a79bb6a9b35f0217e363cc
Alberto Vigna <betoveluis@gmail.com>
2024-11-14 15:42:21 -0500

    Added a panel in the hero component.

--- JaveCompensa-FrontEnd/src/app/components/extras/hero/hero.component.ts
+++ JaveCompensa-FrontEnd/src/app/components/extras/hero/hero.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit, OnDestroy {
   slides = [
-    { image: 'assets/img/hero/HombreFeliz.jpg', alt: 'Foto 1', buttonText: 'Inicia Sesión', link: '/section1' },
-    { image: 'assets/img/hero/Tejo.jpg', alt: 'Foto 2', buttonText: 'Ir a Sección 2', link: '/section2' },
-    { image: 'assets/img/hero/Piscina.jpeg', alt: 'Foto 3', buttonText: 'Ir a Sección 3', link: '/section3' },
+    { image: 'assets/img/hero/HombreFeliz.jpg', alt: 'Foto 1', title: 'Título 1', buttonText: 'Ver Más', link: '/section1' },
+    { image: 'assets/img/hero/Tejo.jpg', alt: 'Foto 2', title: 'Título 2', buttonText: 'Ver Más', link: '/section2' },
+    { image: 'assets/img/hero/Piscina.jpeg', alt: 'Foto 3', title: 'Título 3', buttonText: 'Ver Más', link: '/section3' },
   ];
   currentIndex = 0;
   interval: any;
