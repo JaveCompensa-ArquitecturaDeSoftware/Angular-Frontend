3c5e5cae9283eda06bdae70594da77904d4cc00f
Alberto Vigna <betoveluis@gmail.com>
2024-11-14 23:35:12 -0500

    Created routing to sedes-menu.

--- JaveCompensa-FrontEnd/src/app/app-routing.module.ts
+++ JaveCompensa-FrontEnd/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { PaginaErrorComponent } from './components/pagina-error/pagina-error.component';
 import { HomeComponent } from './components/home/home.component';
+import {SedesMenuComponent} from "./components/sedes-menu/sedes-menu.component";
 
 const routes: Routes = [
   { path: 'error', component: PaginaErrorComponent},
   { path: 'home', component: HomeComponent },
+  { path: 'sedes-menu', component: SedesMenuComponent },
   { path: '**', component: HomeComponent }
 ];
 
