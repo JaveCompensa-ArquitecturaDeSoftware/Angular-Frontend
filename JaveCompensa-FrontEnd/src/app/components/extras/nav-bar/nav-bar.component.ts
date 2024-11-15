d418783bbee95a858f3f3e5e040ce90d3b8f8d00
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2024-11-15 13:57:22 -0500

    Login corregido

--- JaveCompensa-FrontEnd/src/app/components/extras/nav-bar/nav-bar.component.ts
+++ JaveCompensa-FrontEnd/src/app/components/extras/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { KeycloakService } from '../../../authentication/keycloak/keycloak.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserProfile } from '../../../authentication/keycloak/user-profile';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +9,46 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  userProfile: UserProfile | undefined;
+  public loginButtonText = 'LogIn/Registrarse';
+  public showDropdown = false;
 
+  constructor(
+    private keycloakService: KeycloakService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    console.log("Buscando si esta logeado...");
+    this.userProfile = this.keycloakService.profile;
+    if (this.userProfile != undefined) {
+      if (this.userProfile.firstName) {
+        this.loginButtonText = this.userProfile.firstName;
+      }
+    }
+  }
+
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
+
+  checkLogin() {
+    if (this.userProfile == undefined) {
+        console.log("no estás logeado, redireccionando a login");
+        this.router.navigate(['/login']).then(() => {
+            this.keycloakService.init();
+        });
+    }
+}
+
+
+  logout() {
+    if (!this.keycloakService.keycloak) {
+      console.error('Keycloak instance is not available for logout');
+      return;
+    }
+    return this.keycloakService.logout();
+  }
+  
 }
